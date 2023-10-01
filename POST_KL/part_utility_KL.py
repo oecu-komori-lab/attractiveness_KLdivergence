42de4864a24fb09259019e91f84e8d0ce6b1168b
terajiteppei <mi22a007@oecu.jp>
2023-10-01 22:24:09 +0900

    fix: delete pred_mu_cov and add def KLdivergence

--- POST_KL/part_utility_KL.py
+++ POST_KL/part_utility_KL.py
@@ -2,33 +2,23 @@ import numpy as np
 import torch
 import pandas as pd
 import os
-from KL_function import KLdivergence
-from botorch.models import PairwiseGP
 
-def pred_mu_cov(session, name_list): # KL用の効用の推定
+def KLdivergence(Mu1, S1, Mu2, S2): # Calculate KL divergence
     
-    result = torch.load("./data/main_result_data.pt") # 参加者resultデータの読み込み
+    Mu1 = np.around(Mu1, decimals=6, out=None)
+    Mu2 = np.around(Mu2, decimals=6, out=None)
+    S1 = np.around(S1, decimals=6, out=None)
+    S2 = np.around(S2, decimals=6, out=None)
     
-    for name in name_list:
-        print("name:" ,name)
-        result_dir = "./result/" + session + "/" + name + "/"
-        
-        if not os.path.isdir(result_dir):
-            os.makedirs(result_dir ,exist_ok=True)
-            
-        response = torch.load("../data/" + session + "/" + name + '/' + name + "_response.pt") 
-        model = PairwiseGP(result, response)
-        model.load_state_dict(torch.load("../POST_meshgrid/result/model/" + session + "/" + name + "/" + name + "_model_state.pth")) #modelの読み込み
+    D = Mu1.shape[0] # Number of predicted means
+    S2inv = np.linalg.inv(S2) # Compute the inverse of S2
+    tmp = S2inv @ S1 # Matrix product of the inverse of S2 and S1
+    Trace = np.trace(tmp) # Sum of the diagonal elements
+    _ , logdet = np.linalg.slogdet(tmp)
+    error = (Mu1 - Mu2).T @ S2inv @ (Mu1 - Mu2) # Transpose of (mean1 - mean2) @ inverse of S2 @ (mean1 - mean2)
+    KL = 0.5 * (Trace - logdet + error - D)
+    return KL
 
-        all_exKL_mu = model.posterior(result).mean.squeeze().unsqueeze(dim=1) # 予測平均
-        all_exKL_sigma2 = model.posterior(result).variance.squeeze().unsqueeze(dim=1) #　予測共分散
-        all_exKL_cov = model.posterior(result).covariance #　予測分散
-        
-        torch.save(all_exKL_mu, result_dir + name + "_all_PC_exKL_mu.pt")
-        torch.save(all_exKL_sigma2, result_dir + name + "_all_PC_exKL_sigma2.pt")
-        torch.save(all_exKL_cov, result_dir + name + "_all_PC_exKL_covariance.pt")
-        
-        
 def part_all_KL(sessions, name_list):
     save_dir = "./result/part_KL/"
     if not os.path.isdir(save_dir):
@@ -59,7 +49,6 @@ def part_all_KL(sessions, name_list):
     KL_all_list[id+1,0] = cute_to_beauty_KL
     KL_all_list[id+1,1] = beauty_to_cute_KL
 
-    print(name_list)
     index_name = name_list.tolist() + ["all"]
     df = pd.DataFrame(KL_all_list, index=index_name, columns=['CUTENESS to BEAUTY','BEAUTY to CUTENESS'])
     df.to_csv(save_dir + "part_KL_all_mean_KL.csv", float_format="%.6f")
@@ -67,8 +56,6 @@ def part_all_KL(sessions, name_list):
 
 if __name__=="__main__":
     SESSIONS = ["beauty","cute"]
-    name_list = np.loadtxt("../data/name_list.csv", dtype="unicode")
-    for session in SESSIONS:
-        pred_mu_cov(session,name_list)
+    name_list = np.loadtxt("../data/name_list.csv", dtype="unicode") 
         
     part_all_KL(SESSIONS,name_list)
